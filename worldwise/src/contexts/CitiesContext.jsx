b5dfd5287dc1b5d4bdd88b7a5bd64be21f968fe9
Daniel <d.kopac@gmail.com>
2025-12-22 14:09:34 +0100

    Transitioned from useState to useReducer to handle fake API cities data + implemented fake authentication

--- worldwise/src/contexts/CitiesContext.jsx
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -1,24 +1,83 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 const BASE_URL = "http://localhost:9000";
 
 const CitiesContext = createContext();
 
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+  error: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "loadCities":
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+    case "getCity":
+      return {
+        ...state,
+        isLoading: false,
+        currentCity: action.payload,
+      };
+    case "addCity":
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload,
+      };
+    case "deleteCity":
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
+      };
+    case "rejected":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+
 function CitiesProvider({ children }) {
-  const [currentCity, setCurrentCity] = useState({});
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [currentCity, setCurrentCity] = useState({});
+  // const [cities, setCities] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(() => {
     async function loadCities() {
       try {
-        setIsLoading(true);
+        dispatch({ type: "loading" });
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
-        setCities(data);
+        dispatch({ type: "loadCities", payload: data });
       } catch (err) {
         console.error(err);
       } finally {
-        setIsLoading(false);
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading data",
+        });
       }
     }
 
@@ -26,9 +85,13 @@ function CitiesProvider({ children }) {
   }, []);
 
   async function getCity(id) {
-    try {
-      setIsLoading(true);
+    // if city we wanna load is the current city (no need to load)
+    // id is string, currentCity.id is a number
+    if (Number(id) === currentCity.id) return;
 
+    dispatch({ type: "loading" });
+
+    try {
       const res = await fetch(`${BASE_URL}/cities/${id}`);
 
       if (!res.ok) {
@@ -36,17 +99,15 @@ function CitiesProvider({ children }) {
       }
 
       const data = await res.json();
-      setCurrentCity(data);
+      dispatch({ type: "getCity", payload: data });
     } catch (err) {
       console.error(err.message);
-    } finally {
-      setIsLoading(false);
     }
   }
 
   async function createCity(newCity) {
     try {
-      setIsLoading(true);
+      dispatch({ type: "loading" });
 
       const res = await fetch(`${BASE_URL}/cities/`, {
         method: "POST",
@@ -55,11 +116,23 @@ function CitiesProvider({ children }) {
       });
 
       const data = await res.json();
-      setCities((cities) => [...cities, data]);
+      dispatch({ type: "addCity", payload: data });
     } catch {
-      alert("There was an error");
-    } finally {
-      setIsLoading(false);
+      alert("There was an error adding a city");
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      dispatch({ type: "loading" });
+
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+
+      dispatch({ type: "deleteCity", payload: id });
+    } catch {
+      alert("There was an error deleting a city");
     }
   }
 
@@ -69,8 +142,10 @@ function CitiesProvider({ children }) {
         cities,
         isLoading,
         currentCity,
+        error,
         getCity,
         createCity,
+        deleteCity,
       }}
     >
       {children}
--- worldwise/src/pages/Login.jsx
+++ worldwise/src/pages/Login.jsx
@@ -1,16 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  // PRE-FILL FOR DEV PURPOSES
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { isAuthenticated, login } = useAuth();
+
+  function handleLogin(e) {
+    e.preventDefault();
+    login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated === true) {
+        navigate("/app");
+      }
+    },
+    [isAuthenticated, navigate]
+  );
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form
+        className={styles.form}
+        onSubmit={(e) => {
+          handleLogin(e);
+        }}
+      >
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
