d80291d69bde66e8be953e7ac8eb1ba7eb639e43
Daniel <d.kopac@gmail.com>
2025-12-22 14:17:55 +0100

    Fixed CountryList bug

--- worldwise/src/components/CountryList.jsx
+++ worldwise/src/components/CountryList.jsx
@@ -2,8 +2,11 @@ import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
 import CountryItem from "./CountryItem";
+import { useCities } from "../contexts/CitiesContext";
+import { EmojiToPng } from "../utils/EmojiToPng";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
@@ -14,7 +17,10 @@ function CountryList({ cities, isLoading }) {
   const countries = cities.reduce(
     (arr, city) => {
       if (!arr.map((el) => el.country).includes(city.country)) {
-        return [...arr, { country: city.country, emoji: city.emoji }];
+        return [
+          ...arr,
+          { country: city.country, emoji: EmojiToPng(city.emoji) },
+        ];
       } else {
         return arr;
       }
